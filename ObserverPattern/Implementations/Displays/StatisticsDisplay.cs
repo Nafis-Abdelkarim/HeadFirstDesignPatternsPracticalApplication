d5210de76ba74fb7d56485814a1b8a073cdc7578
Nafis-Abdelkarim <kazcrb@gmail.com>
2025-08-02 19:58:09 +0100

    Adding StatisticsDisplay implementation

---
+++ ObserverPattern/Implementations/Displays/StatisticsDisplay.cs
@@ -0,0 +1,43 @@
+﻿using ObserverPattern.Interfaces;
+
+namespace ObserverPattern.Implementations.Displays
+{
+    public class StatisticsDisplay : IObserver, IDisplayElement
+    {
+        private float maxTemp = float.MaxValue;
+        private float minTemp = float.MinValue;
+        private float tempSum = 0f;
+        private int numReadings;
+        private ISubject weatherData;
+
+        public StatisticsDisplay(ISubject weatherData)
+        {
+            this.weatherData = weatherData;
+            weatherData.RegisterObserver(this);
+        }
+
+        public void Update(float temperature, float humidity, float pressure)
+        {
+            tempSum += temperature;
+            numReadings++;
+
+            if (temperature > maxTemp) 
+            {
+                maxTemp = temperature;
+            }
+
+            if (temperature < minTemp) 
+            {
+                minTemp = temperature;
+            }
+
+            Display();
+        }
+
+        public void Display()
+        {
+            float avgTemp = tempSum / numReadings;
+            Console.WriteLine($"Avg/Max/Min temperature = {avgTemp:0.0}/{maxTemp}/{minTemp}");
+        }
+    }
+}
